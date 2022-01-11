52c7748ac7c8a96195c60801476b6530fad1e095
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-01-11 14:13:46 -0500

    adding input and label

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-Hello Youtube
+    <div>
+      <label>Name:</label>
+      <input type="text" value=""></input>
+      <label>Age:</label>
+      <input type="number" value=""></input>
+      <label>Country:</label>
+      <input type="text" value=""></input>
+      <label>Position:</label>
+      <input type="text" value=""></input>
+      <label>Education:</label>
+      <input type="text" value=""></input>
+      <label>Annual Salary:</label>
+      <input type="number" value=""></input>
+      </div>
     </div>
   );
 }
