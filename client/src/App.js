af7ec09e61943f1b27cf44970bd1da4064dae626
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2022-01-12 01:31:44 -0500

    updating and deleting

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,8 @@ function App() {
   const [education, setEducation] = useState("");
   const [position, setPosition] = useState("");
   const [wage, setWage] = useState(0);
-  const [employeeList,setEmployeeList]=useState([])
+  const [employeeList, setEmployeeList] = useState([]);
+  const [newWage, setNewWage] = useState(0);
 
   const handleName = (event) => {
     setName(event.target.value);
@@ -35,33 +36,78 @@ function App() {
     setEducation(event.target.value);
   };
 
-  const addEmployee = () => {
-    axios.post("http://localhost:8000/create", {
-      //second or white or value name,age,position etc refer to value in input
-      //yellow or key or first name, age, education refers to req.body.name in server
-      name: name,
-      age: age,
-      position: position,
-      country: country,
-      education: education,
-      wage: wage,
-    }).then(()=>{
-      console.log("success")
-    }).catch((error) => {
-      console.log(error);
-    });
+  const addEmployee = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8000/create", {
+        //second or white or value name,age,position etc refer to value in input
+        //yellow or key or first name, age, education connected to req.body.name in server
+        name: name,
+        age: age,
+        position: position,
+        country: country,
+        education: education,
+        wage: wage,
+      })
+      .then(() => {
+        console.log("success");
+        // setEmployeeList([
+        //   ...employeeList,
+        //   {
+        //     name: name,
+        //     age: age,
+        //     position: position,
+        //     country: country,
+        //     education: education,
+        //     wage: wage,
+        //   },
+        // ]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
-  const getEmployees=()=>{
-    axios.get("http://localhost:8000/employees", {
-
-    }).then((result)=>{
-      console.log(result)
-    }).catch((error) => {
-      console.log(error);
-    });
+  const getEmployees = () => {
+    axios
+      .get("http://localhost:8000/employees", {})
+      .then((result) => {
+        console.log(result);
+        setEmployeeList(result.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
+  const updateWage = (id) => {
+    axios
+      .put("http://localhost:8000/update", { wage: newWage, id: id })
+      .then((result) => {
+        setEmployeeList(
+          employeeList.map((employee) => {
+            return employee.id === id
+              ? {
+                  id: employee.id,
+                  name: employee.name,
+                  age: employee.age,
+                  position: employee.position,
+                  education: employee.education,
+                  wage: employee.newWage,
+                  country: employee.country
+                }
+              : employee;
+          })
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const deleteEmployees=(id)=>{
+    axios.delete("http://localhost:8000/delete/id")
+  }
 
   return (
     <div className="app">
@@ -113,7 +159,47 @@ function App() {
         </button>
         <hr className="line" />
         <div className="employees">
-        <button onClick={getEmployees} className="information__button"> Show Employees</button>
+          <button onClick={getEmployees} className="information__button">
+            {" "}
+            Show Employees
+          </button>
+        </div>
+        <div>
+          {employeeList.map((employee, key) => {
+            return (
+              <div className="employee">
+                <p className="employee__label--small">Name: {employee.name}</p>
+                <p className="employee__label--small">Age: {employee.age}</p>
+                <p className="employee__label">Position: {employee.position}</p>
+                <p className="employee__label">
+                  Education: {employee.education}
+                </p>
+                <div>
+                  <p className="employee__label--small">
+                    Wage: {employee.wage}
+                  </p>
+                  <input
+                    onChange={(event) => {
+                      setNewWage(event.target.value);
+                    }}
+                    type="text"
+                    placeholder="0"
+                  ></input>
+                  <button
+                    onClick={() => {
+                      updateWage(employee.id);
+                    }}
+                  >
+                    Update
+                  </button>
+                  <button onClick={deleteEmployees(employee.id)}>Delete</button>
+                </div>
+                <p className="employee__label--small">
+                  Country: {employee.country}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
-const cors= require('cors')
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const db = mysql.createConnection({
   user: "root",
@@ -36,8 +36,7 @@ app.post("/create", (req, res) => {
 });
 
 app.get("/employees", (req, res) => {
-
-  db.query("SELECT * FROM employees",(err, result) => {
+  db.query("SELECT * FROM employees", (err, result) => {
     if (err) {
       console.log(err);
     } else {
@@ -45,6 +44,26 @@ app.get("/employees", (req, res) => {
     }
   });
 });
+
+app.put("/update", (req, res) => {
+  const id = req.body.id;
+  const wage = req.body.wage;
+  db.query(
+    "UPDATE employees SET wage = ? WHERE id = ?",
+    [wage, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+// app.delete(()=>{
+
+// })
 
 app.listen(8000, () => {
   console.log("Server is running on PORT 8000");
